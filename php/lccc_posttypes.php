5afaa2a6ff510e9c8ae286893f6666fc0979ed45
Joe Querin <joecue@gmail.com>
2016-09-07 15:03:06 -0400

    Added Post Type Filtering to Admin List

--- php/lccc_posttypes.php
+++ php/lccc_posttypes.php
@@ -145,5 +145,38 @@ function be_register_taxonomies() {
 	
 }
 add_action( 'init', 'be_register_taxonomies' );
-					
+
+
+/*
+ *
+ * Add filtering support to Admin list for the LCCC Event Custom Post Type.
+ *
+ */
+
+function lc_event_cpt_add_taxonomy_filters() {
+	global $typenow;
+ 
+	// an array of all the taxonomyies you want to display. Use the taxonomy name or slug
+	$taxonomies = array('where_to_display','event_categories');
+ 
+	// must set this to the post type you want the filter(s) displayed on
+	if( $typenow == 'lccc_events' ){
+ 
+		foreach ($taxonomies as $tax_slug) {
+			$tax_obj = get_taxonomy($tax_slug);
+			$tax_name = $tax_obj->labels->name;
+			$terms = get_terms($tax_slug);
+			if(count($terms) > 0) {
+				echo "<select name='$tax_slug' id='$tax_slug' class='postform'>";
+				echo "<option value=''>Show All $tax_name</option>";
+				foreach ($terms as $term) { 
+					echo '<option value='. $term->slug, $_GET[$tax_slug] == $term->slug ? ' selected="selected"' : '','>' . $term->name .' (' . $term->count .')</option>'; 
+				}
+				echo "</select>";
+			}
+		}
+	}
+}
+add_action( 'restrict_manage_posts', 'lc_event_cpt_add_taxonomy_filters' );
+
 ?>

bdda508453c139677c49ac8d557b21948e2b38ea
Joe Querin <joecue@gmail.com>
2017-08-10 17:19:13 -0400

    Added ALT Text to Images

--- php/lccc-announcement-rest-widget.php
+++ php/lccc-announcement-rest-widget.php
@@ -143,7 +143,7 @@ class LCCC_Announcement_Feed_Widget extends WP_Widget {
         if ($post->better_featured_image->media_details->sizes->thumbnail->source_url != ''){
          echo '<div class="small-12 medium-3 large-3 columns eventhumbnail">';
          if($post->announcement_altlink != ''){
-         echo '<a href="' . $post->announcement_altlink . '"><img src="' . $post->better_featured_image->media_details->sizes->thumbnail->source_url .'" border="0"></a>';
+         echo '<a href="' . $post->announcement_altlink . '"><img src="' . $post->better_featured_image->media_details->sizes->thumbnail->source_url .'" alt="' . $post->title->rendered . '" border="0"></a>';
          }else{
          echo '<a href="' . $post->link . '"><img src="' . $post->better_featured_image->media_details->sizes->thumbnail->source_url .'" border="0"></a>';
          }
